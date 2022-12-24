ff039f765b0ba4a8182c206557b0b658c90b21eb
Daria Kovylina <dashakvylina60@yandex.ru>
2022-12-24 15:11:30 +0300

    origin

--- backend/app.js
+++ backend/app.js
@@ -17,17 +17,17 @@ const { PORT = 3001 } = process.env;
 
 const app = express();
 
-const whitelist = ['http://localhost:3000', 'https://domainname.students.dasha.nomoredomains.club'];
+// const whitelist = ['http://localhost:3000', 'https://domainname.students.dasha.nomoredomains.club'];
 
 const corsOptions = {
   credentials: true,
-  origin(origin, callback) {
-    if (whitelist.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
+  // origin(origin, callback) {
+  //   if (whitelist.indexOf(origin) !== -1) {
+  //     callback(null, true);
+  //   } else {
+  //     callback(new Error('Not allowed by CORS'));
+  //   }
+  // },
 };
 
 app.use(cors(corsOptions));
